a68b8520965d2aa5e7b85f6755f6a7126e4a6927
mjgfoster <mjgfoster@gmail.com>
2026-03-11 12:53:32 -0500

    Restructure newsletter article cards for vertical layout
    - Change card wrapper from event-card to tclas-card with vertical stacking
    - Image on top, content below (.tclas-card__body)
    - Move image link to tabindex=-1 aria-hidden (pattern consistency)
    - Simplify meta layout: issue date · By author
    - Update class names from tclas-event-card__* to tclas-card__*
    - Aligns with post card component structure for consistency

--- wp-content/themes/clausen/template-parts/content/card-article-nl.php
+++ wp-content/themes/clausen/template-parts/content/card-article-nl.php
@@ -19,39 +19,36 @@ if ( $issue_date ) {
 $author = get_the_author();
 $author_url = get_author_posts_url( get_the_author_meta( 'ID' ) );
 ?>
-<a href="<?php the_permalink(); ?>" class="tclas-event-card">
-	<article>
-		<div class="tclas-event-card__image">
+<article class="tclas-card tclas-card--accented">
+	<div class="tclas-card__image">
+		<a href="<?php the_permalink(); ?>" tabindex="-1" aria-hidden="true">
 			<?php if ( has_post_thumbnail() ) : ?>
 				<?php the_post_thumbnail( 'tclas-card', [ 'loading' => 'lazy', 'alt' => '' ] ); ?>
 			<?php else : ?>
-				<div class="tclas-event-card__image-placeholder" aria-hidden="true">
+				<div class="tclas-card__image--placeholder" aria-hidden="true">
 					📰
 				</div>
 			<?php endif; ?>
+		</a>
+	</div>
+
+	<div class="tclas-card__body">
+		<div class="tclas-card__meta">
+			<?php if ( $issue_label ) : ?>
+				<span><?php echo esc_html( $issue_label ); ?></span>
+				<span aria-hidden="true">&middot;</span>
+			<?php endif; ?>
+			<?php if ( $author ) : ?>
+				<span><?php esc_html_e( 'By', 'tclas' ); ?> <a href="<?php echo esc_url( $author_url ); ?>" class="tclas-card__meta-link"><?php echo esc_html( $author ); ?></a></span>
+			<?php endif; ?>
 		</div>
 
-		<div class="tclas-event-card__content">
-			<h3 class="tclas-event-card__title">
-				<?php the_title(); ?>
-			</h3>
+		<h3 class="tclas-card__title">
+			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		</h3>
 
-			<p class="tclas-event-card__excerpt">
-				<?php echo esc_html( wp_trim_words( get_the_excerpt(), 22 ) ); ?>
-			</p>
-
-			<div class="tclas-event-card__meta">
-				<?php if ( $issue_label ) : ?>
-					<div class="tclas-event-card__meta-row">
-						<span><?php echo esc_html( $issue_label ); ?></span>
-					</div>
-				<?php endif; ?>
-				<?php if ( $author ) : ?>
-					<div class="tclas-event-card__meta-row">
-						<span><?php esc_html_e( 'By', 'tclas' ); ?> <a href="<?php echo esc_url( $author_url ); ?>" class="tclas-event-card__meta-link"><?php echo esc_html( $author ); ?></a></span>
-					</div>
-				<?php endif; ?>
-			</div>
-		</div>
-	</article>
-</a>
+		<p class="tclas-card__excerpt">
+			<?php echo esc_html( wp_trim_words( get_the_excerpt(), 22 ) ); ?>
+		</p>
+	</div>
+</article>
